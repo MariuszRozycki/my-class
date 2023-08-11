bfdce8eec696164b55bc08ec8193010c26dccd1c
MariuszRozycki <mariansc83@icloud.com>
2023-08-11 19:07:38 +0200

    Create function filterPosts

--- js/posts/index.mjs
+++ js/posts/index.mjs
@@ -1,12 +1,14 @@
 /* imported functions */
 import { createNewPost } from "./createNewPost.mjs";
 import { getPosts } from "./getPosts.mjs";
+import { filterPost } from "./filterPosts.mjs";
 
 const path = location.pathname;
 console.log(path);
 
 if (path === `/pages/feed/`) {
   getPosts();
+  filterPost();
 } else if (path === `/pages/create-post/`) {
   createNewPost();
 }
