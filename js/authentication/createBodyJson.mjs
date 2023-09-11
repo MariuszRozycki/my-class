d09398159668f4d2cc386cc9de560e02b23c9d46
MariuszRozycki <mariansc83@icloud.com>
2023-09-11 22:48:56 +0200

    Add JSDocs to createBodyJson

--- js/authentication/createBodyJson.mjs
+++ js/authentication/createBodyJson.mjs
@@ -1,3 +1,19 @@
+/**
+ * Creates a JSON body for an HTTP request based on the given method and data.
+ * 
+ * @function createBodyJson
+ * @param {string} method - The HTTP method ('GET', 'PUT', 'POST', 'DELETE').
+ * @param {Object} [data] - Optional data to be converted to JSON string.
+ * @returns {string|null} - A JSON string if method is not 'GET' and data is provided, otherwise null.
+ * 
+ * @example  
+ * // Example usage:
+ * const method = 'POST';
+ * const data = { key: 'value' };
+ * const body = createBodyJson(method, data); 
+ * Returns '{"key":"value"}'
+ */
+
 export function createBodyJson(method, data) {
   let body;
   if (method !== 'GET' && data) {
