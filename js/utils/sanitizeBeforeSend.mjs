d86a8b7ee609b71bd5abf218ddfede0ecdb7ce7a
MariuszRozycki <mariansc83@icloud.com>
2023-09-14 07:40:48 +0200

    Add JSDoc to sanitizeBeforeSend.mjs

--- js/utils/sanitizeBeforeSend.mjs
+++ js/utils/sanitizeBeforeSend.mjs
@@ -1,3 +1,18 @@
+/**
+ * Sanitizes a string by encoding certain characters and removing forbidden keywords.
+ *
+ * This function encodes special HTML characters like `&`, `<`, `>`, `"`, and `'` into their respective HTML entities.
+ * It also removes potentially dangerous keywords such as 'onerror', 'onload', 'script', 'img', and 'body'.
+ *
+ * @param {string} str - The input string to sanitize.
+ * @returns {string} - The sanitized string.
+ * @example
+ * // Example usage
+ * const unsafeString = 'Hello <script>onerror=evil()</script>';
+ * const safeString = sanitizeBeforeSend(unsafeString);
+ * // safeString will be 'Hello '
+ */
+
 export function sanitizeBeforeSend(str) {
   let sanitized = str.replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
