bd95312b027eeca57ea67b0c119e81021b1e16b5
MariuszRozycki <mariansc83@icloud.com>
2023-09-14 07:24:25 +0200

    Add JSDoc to saniizeBeforeRender.mjs

--- js/utils/sanitizeBeforeRender.mjs
+++ js/utils/sanitizeBeforeRender.mjs
@@ -1,3 +1,18 @@
+/**
+ * Sanitizes a string containing HTML to remove potentially malicious code.
+ * 
+ * This function removes <script> and <style> tags, HTML comments, and other HTML tags. 
+ * It also removes certain potentially dangerous attributes like `onerror`, `onload`, `onclick`, and `onmouseover`.
+ *
+ * @param {string} data - The string containing HTML to sanitize.
+ * @returns {string} - The sanitized HTML string.
+ * @example
+ * // Example usage
+ * const unsafeHTML = '<script>alert("Hacked!");</script><p onclick="malicious()">Click me</p>';
+ * const safeHTML = sanitizeBeforeRender(unsafeHTML);
+ * // safeHTML will be '<p>Click me</p>'
+ */
+
 export function sanitizeBeforeRender(data) {
   let sanitized = data.replace(/<script[^>]*?>.*?<\/script>/gi, '')
     .replace(/<[\/\!]*?[^<>]*?>/gi, '')
